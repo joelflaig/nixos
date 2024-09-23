731c36cb95cff57222854d22714f2074ee0ef29d
joelflaig <j.flaig@progymnasium-alpirsbach.de>
2024-09-23 18:49:21 +0200

    pkgs: Copied all packages from dotfiles repo (and added a few)

--- configuration.nix
+++ configuration.nix
@@ -62,9 +62,129 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # well, idk
+    vim
     wget
-    git
+
+    # audio
+    pipewire # pipewire -- audio
+    wireplumber # session/policy manager for PipeWire
+
+    # hyprland
+    hyprland # hyprland -- window manager
+    hyprpicker # hyprpicker -- color picker
+    xdg-desktop-portal-hyprland # xdg-desktop-portal-hyprland -- xdg-desktop-portal backend for hyprland
+
+    # bootloader
+    grub2 # grub -- bootloader
+
+    # boot
+    plymouth # plymouth -- boot splash/logger
+
+    # display manager
+    sddm # sddm -- display manager
+
+    # dotfiles
+    stow
+
+    # terminal
+    kitty # terminal
+    zsh # shell
+    tmux # tmux -- terminal multiplexer
+    starship # starship -- shell prompt
+    zsh-syntax-highlighting # zsh-syntax-highlighting -- syntax highlighting for zsh
+    zsh-autosuggestions # zsh-autosuggestions -- suggestions for zsh
+    zsh-autopair # zsh-autopair -- brackets, quotes, etc.
+    zsh-vi-mode # zsh-vi-mode -- better vi mode
+    thefuck # thefuck -- corrects command mistakes
+
+    # rice
+    pipes # pipes.sh -- animated pipes terminal screensaver
+    cmatrix # cmatrix -- matrix effect
+
+    # languages
+    libgcc # gcc -- C/C++ compiler
+    llvm_18 # llvm -- compiler framework
+    lua # lua -- lua interpreter
+    nodejs_22 # nodejs -- javascript
+    rustup # rustup -- rust language
+    go # go -- golang
+
+    # editing
+    neovim # neovim -- editor
+
+    # git
+    git # git -- version management
+    lazygit # lazygit -- git client
+    delta # delta -- diff tool
+    gh # gh -- github cli
+    git-lfs # git-lfs -- git large file storage
+
+    # finders
+    fzf # fzf -- fuzzy finder
+    fd # fd -- find files
+    ripgrep # ripgrep -- grep replacement
+
+    # files && folders
+    yazi # yazi -- file manager
+    zoxide # zoxide -- cd replacement
+    stow # stow -- symlink manager
+    bat # bat -- cat with syntax highlighting
+    eza # eza -- ls replacement
+
+    # misc
+    sl # sl -- steam locomotive
+    yq # yq -- yaml, json, xml processor
+    btop # btop++ -- command line task manager
+    man # man -- docs
+
+    # web
+    firefox # firefox -- web browser
+
+    # notes
+    obsidian # obsidian -- note taking
+
+    # recording
+    obs-studio # obs studio -- screen recording
+
+    # notifications
+    dunst # dunst -- notification daemon
+    libnotify # libnotify -- notify-send
+
+    # authentication
+    kdePackages.polkit-kde-agent-1
+
+    # discord -- chat
+    discord
+
+    # clipboard
+    wl-clipboard # wl-clipboard -- clipboard manager
+    wl-clip-persist # wl-clip-persist -- keep wl-clipboard after program closes
+    clipse # clipse -- tui clipboard manager
+
+    # automount
+    udiskie # udiskie -- automount usb drives
+
+    # zip
+    unzip # unzip -- unzip tool
+
+    # fetch
+    neofetch # neofetch -- system information tool
+
+    # clock
+    gnome.gnome-clocks # gnome-clocks -- clock
+
+    # speech
+    speechd
+
+    # images
+    loupe # loupe -- image viewer
+
+    # wallpaper
+    swww # swww -- wallpaper manager
+
+    # office
+    wpsoffice # wps office -- Free MS compatible office suite
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -95,3 +215,4 @@
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
+
